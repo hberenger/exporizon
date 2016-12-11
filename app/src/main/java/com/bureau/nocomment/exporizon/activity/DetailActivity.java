63a4d954073db30e9f8f062c18543cd4297086a4
Hervé Bérenger <herve.berenger@gmail.com>
2016-12-11 16:47:42 +0100

    Add interface elements to the Detail Activity

--- app/src/main/java/com/bureau/nocomment/exporizon/activity/DetailActivity.java
+++ app/src/main/java/com/bureau/nocomment/exporizon/activity/DetailActivity.java
@@ -7,23 +7,42 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bureau.nocomment.exporizon.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class DetailActivity extends AppCompatActivity {
+
+    @Bind(R.id.item_image) ImageView itemImage;
+    @Bind(R.id.item_title) TextView itemTitle;
+    @Bind(R.id.item_subtitle) TextView itemSubtitle;
+    @Bind(R.id.item_description) TextView itemDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        itemImage.setImageResource(R.drawable.weininger);
+        itemTitle.setText("Théâtre sphérique");
+        itemSubtitle.setText("Andor Weininger, 1926");
+        itemDescription.setText(R.string.lorem_ipsum);
+        itemDescription.setMovementMethod(new ScrollingMovementMethod());
     }
 
     @Override
